150098ad83acc21d9553cad05f2a652327e34897
YDG <zzmn1234@naver.com>
2022-06-13 00:18:53 +0900

    add usehooks

---
+++ useAxios/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import useAxios from './useAxios';
+const App = () => {
+  const { loading, data, refetch } = useAxios({
+    url: 'yts.am/api/v2/list_movies.json',
+  });
+
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && 'Loading'}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+};
+
+export default App;
